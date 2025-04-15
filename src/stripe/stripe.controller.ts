ef8ba36440948b7faf58b5619954cbb4abd5dcc7
SDSamarasinghe <it20605902@my.sliit.lk>
2025-04-15 19:53:18 +0530

    feat: add StripeController for handling checkout sessions

---
+++ src/stripe/stripe.controller.ts
@@ -0,0 +1,15 @@
+// src/stripe/stripe.controller.ts
+import { Controller, Post, Body } from '@nestjs/common';
+import { StripeService } from './stripe.service';
+
+@Controller('stripe')
+export class StripeController {
+  constructor(private readonly stripeService: StripeService) {}
+
+  @Post('checkout')
+  createSession(
+    @Body() body: { name: string; price: number; quantity: number },
+  ) {
+    return this.stripeService.createCheckoutSession(body);
+  }
+}
