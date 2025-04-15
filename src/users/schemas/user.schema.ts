ab25266a1ea0b10dcff4c06ab05ee7f104534fbf
SDSamarasinghe <it20605902@my.sliit.lk>
2025-04-15 19:54:27 +0530

    feat: add User schema with properties for user management

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { UserRole } from '../enums/user-role.enum';
+
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true, lowercase: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ type: String, enum: UserRole, default: UserRole.CUSTOMER })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
