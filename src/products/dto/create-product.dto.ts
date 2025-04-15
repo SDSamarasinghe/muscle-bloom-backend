97780bd8e1b2e59127eba762469e654f60961e92
SDSamarasinghe <it20605902@my.sliit.lk>
2025-04-15 19:53:06 +0530

    feat: add CreateProductDto for product creation

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  price: number;
+}
