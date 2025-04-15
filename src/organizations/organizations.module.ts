3482fb3af217a1fd88642f812c1c1f1b6643e7e8
SDSamarasinghe <it20605902@my.sliit.lk>
2025-04-15 20:10:23 +0530

    feat: integrate Mongoose for Organization schema in OrganizationsModule

--- src/organizations/organizations.module.ts
+++ src/organizations/organizations.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { OrganizationsController } from './organizations.controller';
 import { OrganizationsService } from './organizations.service';
+import {
+  Organization,
+  OrganizationSchema,
+} from './schemas/organization.schema';
+import { EmailModule } from '../email/email.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Organization.name, schema: OrganizationSchema },
+    ]), // Register Organization schema
+    EmailModule, // Import EmailModule for EmailService
+  ],
   controllers: [OrganizationsController],
-  providers: [OrganizationsService]
+  providers: [OrganizationsService],
 })
 export class OrganizationsModule {}
